4a5b5b11128c5c9a861b8f25a75aa55805380215
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2023-11-09 12:26:32 -0500

    finished the challenge

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    // "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'storm-grey': '#677489',
+      'mirage': '#111729',
+      'catskill-white': '#F2F5F9',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
